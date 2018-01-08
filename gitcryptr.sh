0e878715894e0613a2a52286b5cc573364cbfcd5
Sun Linjin <lj.sun@mstglobal.com>
2018-01-08 12:13:47 +0800

    initial version, still debugging

--- gitcryptr.sh
+++ gitcryptr.sh
@@ -2,86 +2,85 @@
 
 #####
 # ./gitcryptr <password> <git-repo-link>
-# Inspired by https://gist.github.com/shadowhand/873637
+# updated base on git@github.com:khast3x/gitcryptr.git
 #####
 
-if [[ $# -ne 2 ]] ; then
-    echo "Usage: $0 <password> <git-repo-link>"
-    exit 0
+# openssl smime -encrypt -aes256 -binary -outform DEM -in "$LEAFTMP" -out "$ROOTREPO" "$GITPUBLIC"
+# openssl smime -decrypt -binary -inform DEM -inkey "$GITPRIVATE" -in "$ROOTREPO" -out "$LEAFTMP"
+
+WorkingDir="$(pwd)"
+PrivateKey=${WorkingDir}/RSA_Private
+PublicKey=${WorkingDir}/RSA_Public
+if [ ! -s "$PublicKey" ];then
+    echo "-----BEGIN CERTIFICATE-----
+MIIC1TCCAb2gAwIBAgIJAOu4V9Pmcu1iMA0GCSqGSIb3DQEBCwUAMAAwIBcNMTYx
+MDA2MDI1MjIxWhgPMjI5MDA3MjIwMjUyMjFaMAAwggEiMA0GCSqGSIb3DQEBAQUA
+A4IBDwAwggEKAoIBAQC22nY3Jv9/gtv/gAkekdLKks6yAggHWdee05Q1QfZQ+9Iv
+pUhViFQLV2HHIU8Iwmtg82MbpeX2CncKR+kNT0GaYSbAH3abOGGB1lov1qnE61kg
+dYru1saKNJ3FGw0TcrC7kF77iR/ILd0HFJZt1EF5Yr63dl2ylPv6GlHHkDvS1mPa
+eyFmYdMnZcjCWzEnBbzfoNaZnEfScWP0TD67KQIWTf/kH6Wl9pD03bLh1EDuADkl
+CxiVJFvxtmQTZIHXkWgCnl7/BxpWuVxqlG1rPNvPw6A+b1GOyX2MoybxB9XodQdC
+pzkIm3MN5G3GL/0z0sbSaryHyu2ZCUHRwsjW8RZBAgMBAAGjUDBOMB0GA1UdDgQW
+BBSWOHzXXiF0aS7m4ZiAS+mrbpFfrzAfBgNVHSMEGDAWgBSWOHzXXiF0aS7m4ZiA
+S+mrbpFfrzAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQAgLONi+87Y
+ck6NOSeMlt9HjT2TH9AKfGQTgkKBfYdSfI66635uK8rSgNemNOvSVJwR15AHdUtH
+HgynOp9w0flQHYPMRwBC0f23RISHpH/oB0iM5crHd0tqUZzKdwkWOZxkYGlIDkgl
+hBbX29tTPwzQkjAHXk6YvCUXSTfey9PAZp50hCwEUdx1N7U7eXnWR4L5t3nsmDY0
+iLaYa1OxJj490BZV3Avzbj+dwIuq1ryAb+bly+QOR/VoCBXqxnIofb9p+SLKI3Ll
+B6gO0kh4k2z6mu+4vLtdNd0VrGLW8pXrcJXCSR5BwnR2+WB7fE/30pRsevFvhxJZ
+Wvj82M2gHjYY
+-----END CERTIFICATE-----" > "$PublicKey"
+fi
+
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <repo-url>"
+    exit 1
+else
+    REPO_URL=$1
 fi
 
 if [[ $EUID -eq 0 ]]; then
-   echo "WARNING: INITIALISING WITH ROOT PRIV" 1>&2
+   echo "WARNING: Initialising with root Priviledge" 1>&2
 fi
 
-echo "                   .__  __                               __           "
-echo "              ____ |__|/  |_  ___________ ___.__._______/  |________  "
-echo "             / ___\|  \   __\/ ___\_  __ <   |  |\____ \   __\_  __ \ "
-echo "            / /_/  >  ||  | \  \___|  | \/\___  ||  |_> >  |  |  | \/ "
-echo "            \___  /|__||__|  \___  >__|   / ____||   __/|__|  |__|    "
-echo "           /_____/               \/       \/     |__|  t : @kh4st3x   "
-echo ""
-echo ""
-
-
-#####
-# Init User configs.
-# Salt is random 12 hex character alphanumeric string (lowercase only).
-#####
-echo "Starting random salt generation"
-INIT_SALT=$(cat /dev/urandom | tr -dc 'a-fA-F0-9' | fold -w 12 | head -n 1)
-echo "Generated following salt : $INIT_SALT"
-echo ""
-
-INIT_PASS=$1
-GIT_URL=$2
-
-mkdir ".gitcryptr"
-cd ".gitcryptr"
+(
+mkdir ".gitcryptr"; cd ".gitcryptr"
 
 #####
 # Create clean_filter_openssl file AKA encryption.
-# Using ECB encryption to keep timestamps.
-# Change to CBC for stronger encryption, but recreates file at every smudge.
-#####
-echo "#!/bin/bash" > clean_filter_openssl
-echo "SALT_FIXED=$INIT_SALT" >> clean_filter_openssl
-echo "PASS_FIXED=$INIT_PASS" >> clean_filter_openssl
-echo "openssl enc -base64 -aes-256-ecb -S \$SALT_FIXED -k \$PASS_FIXED" >> clean_filter_openssl
+cat << EOF > clean_filter_openssl
+#!/bin/bash
+openssl smime -encrypt -aes256 -binary -outform DEM "$PublicKey" 2> /dev/null  || cat
+EOF
 
 #####
 # Create smudge_filter_openssl AKA decryption
-# Salt is not required for decryption
-#####
-echo "#!/bin/bash" > smudge_filter_openssl
-echo "" >> smudge_filter_openssl
-echo "# No salt is needed for decryption" >> smudge_filter_openssl
-echo "PASS_FIXED=$INIT_PASS" >> smudge_filter_openssl
-echo "" >> smudge_filter_openssl
-echo "# If decryption fails, use cat instead." >> smudge_filter_openssl
-echo "# Error messages are redirected to /dev/null." >> smudge_filter_openssl
-echo "openssl enc -d -base64 -aes-256-ecb -k \$PASS_FIXED 2> /dev/null || cat" >> smudge_filter_openssl
+cat << EOF > smudge_filter_openssl
+#!/bin/bash"
+
+# If decryption fails, use cat instead."
+# Error messages are redirected to /dev/null."
+openssl smime -decrypt -binary -inform DEM -inkey "$PrivateKey" 2> /dev/null || cat
+EOF
 
 #####
 # Create diff_filter_openssl
-# Decryption only, no salt needed
-#####
-echo "#!/bin/bash" > diff_filter_openssl
-echo "" >> diff_filter_openssl
-echo "# No salt is needed for decryption." >> diff_filter_openssl
-echo "PASS_FIXED=$INIT_PASS" >> diff_filter_openssl
-echo "" >> diff_filter_openssl
-echo "# Error messages are redirect to /dev/null." >> diff_filter_openssl
-echo "openssl enc -d -base64 -aes-256-ecb -k \$PASS_FIXED -in "\$1" 2> /dev/null || cat "\$1"" >> diff_filter_openssl
+cat << EOF > diff_filter_openssl
+#!/bin/bash"
 
+# If decryption fails, use cat instead."
+# Error messages are redirected to /dev/null."
+openssl smime -decrypt -binary -inform DEM -inkey "$PrivateKey" -in "\$1" 2> /dev/null || cat "\$1"
+EOF
+)
 #####
 # Link up to remote git
 #####
-cd ..
-chmod "-R" "+rx" ".gitcryptr/"
+chmod -R "+rx" .gitcryptr
 echo "gitcryptr : If you're seeing this message from the git server something went wrong" > README.md
 git init
-git remote add origin $GIT_URL
+git remote add origin $REPO_URL
 
 #####
 # Create .gitattributes defining commit filters
@@ -102,11 +101,11 @@ echo "    textconv = `pwd`/.gitcryptr/diff_filter_openssl" >> .git/config
 #####
 # Exclude files to ensure not pushing password or script
 #####
-echo ""
-echo "-- Excluding encryption configuration from git repository"
-echo ""
+echo -e "\n-- Excluding encryption configuration from git repository\n"
 echo ".gitcryptr/*" > .git/info/exclude
 echo "gitcryptr.sh" >> .git/info/exclude
+
+ls -la .gitcryptr
 
 #####
 # Finish up and push README to master branch
@@ -115,7 +114,5 @@ git add README.md
 git commit -m "Initial gitcryptr commit"
 git push -u origin master
 
-echo "--------"
-echo "gitcryptr is done. Clean your shell history"
-echo "--------"
-echo ""
+clear
+echo -e "\n\ngitcryptr is done. Clean your shell history\n"
